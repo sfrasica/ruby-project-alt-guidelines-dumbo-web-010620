57801042b178b1de44039231ef5f7521e1536685
Lori Boyd <lori.p.boyd@gmail.com>
2020-01-22 11:11:52 -0500

    added a bids_won method to ArtDealer

--- lib/models/art_dealer.rb
+++ lib/models/art_dealer.rb
@@ -1,4 +1,9 @@
 class ArtDealer < ActiveRecord::Base
     has_many :bids
     has_many :work_of_gifs, through: :bids
+
+    def bids_won
+      #returns an array of bids the ArtDealer won
+      self.bids.where(win: true)
+    end
 end
