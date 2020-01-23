38c3a952a8e4ade04b8866e59a186336e87678b6
Lori Boyd <lori.p.boyd@gmail.com>
2020-01-23 13:17:22 -0500

    We cooking

--- lib/models/command_line_interface.rb
+++ lib/models/command_line_interface.rb
@@ -2,11 +2,12 @@ require "tty-prompt"
 ##
 
 class CommandLineInterface
-    attr_accessor :prompt, :user, :art_dealer, :lei
+    attr_accessor :prompt, :user, :art_dealer, :ad, :pw
 
     def initialize()
         @prompt = TTY::Prompt.new
-        @art_dealer = ArtDealer.create
+        @ad = ArtDealer.create(name: "user")
+        @pw = WorkOfGif.all.first
     end
 
     def welcome
@@ -25,9 +26,11 @@ class CommandLineInterface
         case choice
         when "No"
             puts "Excellent, we hope you enjoy this lovely experience."
+
         when "Yes"
             puts "Welcome back."
         end
+
         main_menu
     end
 
@@ -36,18 +39,19 @@ class CommandLineInterface
         system "clear" # Clears the page user is on, like going to the next screen
         # user.reload <- most likely including later
         prompt.select("Welcome prospective ArtDealer! What strikes your fancy?") do |menu|
-            menu.choice "Visit the GifShoppe", -> {WorkOfGif.visit_gif_shoppe}
-            menu.choice "View your bids", -> {lei.view_bids}
-            menu.choice "Peruse your GifGallery", -> {lei.view_your_won_works}
+            menu.choice "Visit the GifShoppe", -> {gif_shoppe}
+            menu.choice "View your bids", -> {ad.view_bids}
+            menu.choice "Peruse your GifGallery", -> {ad.view_your_won_works}
             menu.choice "Exit the GifShoppe", -> {exit_the_gif_shoppe}
         end
         STDIN.getch
         main_menu
     end
 
+
     # def gif_shoppe_menu
       # Maybe we can use prompt.select here
-      # we'd call WOG.all.each_with_index and store it in a variable
+      # we'd call WOG.all.map and store it in a variable
       # so we could call something like menu.choice "gif[0].name", -> {bid menu}
       # and so on
     # end
@@ -65,8 +69,8 @@ class CommandLineInterface
     # def bid_sequence(wog)
     #   # puts "bid amount and ask how much you want to bid"
     #   bid_amount = gets.chomp.to_i
-    #   lei.create_bid(bid_amount, self.id, wog.id, #win defalt to false, #counter defalts to 1)
-    #   puts lei.bids.find_by(wog.id)
+    #   ad.create_bid(bid_amount, self.id, wog.id, #win defalt to false, #counter defalts to 1)
+    #   puts ad.bids.find_by(wog.id)
     # end
 # These should probably go into the gif_shoppe rb file
     def get_seeds
@@ -74,9 +78,9 @@ class CommandLineInterface
         Bid.destroy_all
         WorkOfGif.destroy_all
 
-        @lei = ArtDealer.create(name: "Lei")
         jezebel = ArtDealer.create(name: "Jezebel")
         steven = ArtDealer.create(name: "Steven")
+
 
         #WorkOfGif instances
         pennywise = WorkOfGif.create(name: "Pennywise Dancing")
@@ -86,10 +90,10 @@ class CommandLineInterface
 
 
         #Bid instances  amount,  art_dealer_id,  work_of_gif_id,
-        Bid.create(amount: 100, art_dealer_id: lei.id, work_of_gif_id: pennywise.id, win: true)
-        Bid.create(amount: 100, art_dealer_id: lei.id, work_of_gif_id: baby_yoda.id, win: false)
-        Bid.create(amount: 100, art_dealer_id: lei.id, work_of_gif_id: darth_vader.id, win: false)
-        Bid.create(amount: 100, art_dealer_id: lei.id, work_of_gif_id: captain_falcon.id, win: true)
+        Bid.create(amount: 100, art_dealer_id: ad.id, work_of_gif_id: pennywise.id, win: true)
+        Bid.create(amount: 100, art_dealer_id: ad.id, work_of_gif_id: baby_yoda.id, win: false)
+        Bid.create(amount: 100, art_dealer_id: ad.id, work_of_gif_id: darth_vader.id, win: false)
+        Bid.create(amount: 100, art_dealer_id: ad.id, work_of_gif_id: captain_falcon.id, win: true)
     end
 
     def exit_the_gif_shoppe
--- lib/models/gif_shoppe.rb
+++ lib/models/gif_shoppe.rb
@@ -1,8 +1,41 @@
+require_relative 'work_of_gif'
 # Placeholder for methods in progress
 
 
 # This method is currently in WorkOfGif class as a class method
 # def visit_gif_shoppe
-#     WorkOfGif.all.map {|workofgif| puts workofgif.name} #can view all available WOG 
-# end 
+#     WorkOfGif.all.map {|workofgif| puts workofgif.name} #can view all available WOG
+# end
+pw = WorkOfGif.all.first
+def gif_shoppe
+  prompt.select("Which Gif are you intrested in?") do |menu|
+      menu.choice "Pennywise", -> {pennywise}
+      menu.choice "Exit", -> {lei.view_bids}
+  end
+end
 
+def bid_sequence(wog)
+  puts "Starting bid is: #{wog.starting_bid}"
+  puts "How much would you like to bid?"
+
+  first_bid = gets.chomp.to_i
+  lei.create_bid(bid_amount, self.id, wog.id) #win defalt to false, #counter defalts to 1)
+end
+
+
+
+
+def pennywise
+  puts "Pennywise the dancing clown!"
+  puts "starting bid: #{pw.starting_bid}"
+
+  # pennygif method
+
+  #bid and exit options
+
+  prompt.select("Do you feel lucky?") do |menu|
+      menu.choice "Bid", -> {bid_sequence(pw)}
+      menu.choice "Exit", -> {lei.view_bids}
+  end
+
+end
